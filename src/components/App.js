6f3345b9b6e4600bbf0aeb400b4a428c777c9fbb
Zafar Yakubov <zafar@idrac-natownica-akademik.net.ds.pw.edu.pl>
2021-12-16 21:02:56 +0100

    Added react-router functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navigation from "./Navigation";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
+import {Route, Switch, Link} from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <Navbar collapseOnSelect fixed='top' expand='sm' bg='dark' variant='dark'>
+      <div className={"navbar"}>
+        <div>
+          <Navbar collapseOnSelect fixed='top' expand='sm' bg='dark' variant='dark'>
             <Container>
-                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-                <Navbar.Collapse id='responsive-navbar-nav'>
-                    <Nav>
-                        <Nav.Link href='/'>Home</Nav.Link>
-                        <Nav.Link href='/circuit-sim'>Falstad Circuit Simulator</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+              <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+              <Navbar.Collapse id='responsive-navbar-nav'>
+                <Nav>
+                  <Nav.Link href='/'>Home</Nav.Link>
+                  <Nav.Link href='/circuit-sim'>Falstad Circuit Simulator</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
             </Container>
-        </Navbar>
+          </Navbar>
+        </div>
+        <div>
+          <Switch>
+            <Route exact path='/circuit-sim' component={Simulator} />
+          </Switch>
+        </div>
+      </div>
     )
 }
 
