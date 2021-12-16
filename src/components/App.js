123994054a4b75f3f217f01d47c43c8d7e606faf
Zafar Yakubov <zafar@idrac-natownica-akademik.net.ds.pw.edu.pl>
2021-12-16 20:17:53 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navigation from "./Navigation";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <Navigation></Navigation>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Navbar, Nav, Container} from 'react-bootstrap';
+
+const Navigation = () => {
+    return (
+        <Navbar collapseOnSelect fixed='top' expand='sm' bg='dark' variant='dark'>
+            <Container>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                <Navbar.Collapse id='responsive-navbar-nav'>
+                    <Nav>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                        <Nav.Link href='/circuit-simulator'>Falstad Circuit Simulator</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Navigation;
