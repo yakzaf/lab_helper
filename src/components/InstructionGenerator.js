d6df08a81bcccfae5654c4994008eedd431d9e57
Zafar Yakubov <zafar@idrac-natownica-akademik.net.ds.pw.edu.pl>
2022-02-06 05:50:51 +0100

    Fixed custom directive attributes

--- src/components/InstructionGenerator.js
+++ src/components/InstructionGenerator.js
@@ -2,11 +2,18 @@ import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import directive from "remark-directive";
 import { visit } from "unist-util-visit";
+import { h } from "hastscript/html.js";
 import remarkGfm from "remark-gfm";
+import DataGrid, { TextEditor } from "react-data-grid";
 
 const InstructionGenerator = () => {
   const [inst, setInst] = useState("");
+  const [rowsTest, setRows] = useState([
+    { id: 0, title: "" },
+    { id: 1, title: "" },
+  ]);
 
+  //fetching the instruction file
   useEffect(() => {
     const fetchFile = async () => {
       const file = await import("../instructions/index.md");
@@ -18,29 +25,54 @@ const InstructionGenerator = () => {
     fetchFile().catch(console.error);
   }, []);
 
+  //hook for updating data table
+  useEffect(() => {
+    console.log(rowsTest);
+  }, [rowsTest]);
+
   const reactMarkdownRemarkDirective = () => {
-    return (tree) => {
-      visit(
-        tree,
-        ["textDirective", "leafDirective", "containerDirective"],
-        (node) => {
-          node.data = {
-            hName: node.name,
-            hProperties: node.attributes,
-            ...node.data,
-          };
-          return node;
-        }
-      );
-    };
+    return transform;
+
+    function transform(tree) {
+      visit(tree, ondirective);
+    }
+
+    function ondirective(node) {
+      let data = node.data || (node.data = {});
+      if (
+        node.type === "textDirective" ||
+        node.type === "leafDirective" ||
+        node.type === "containerDirective"
+      ) {
+        let hast = h(node.name, node.attributes);
+        data.hName = node.name;
+        data.hProperties = hast.properties;
+      }
+    }
   };
 
   const customDirectives = {
-    testDirective: ({ attributes }) => {
-      return <input {...attributes} />;
-    },
-    test1Directive: ({ attributes, children }) => {
-      return <span {...attributes} />;
+    dataTable: (attr) => {
+      let cols = attr.columns.slice(1, -1).split(" , ");
+      let colsObj = cols.map((x) => JSON.parse(x));
+      for (let i = 0; i < colsObj.length; i++) {
+        colsObj[i].editable = true;
+        colsObj[i].editor = TextEditor;
+      }
+
+      let rows = attr.rows.slice(1, -1).split(" , ");
+      let rowsObj = rows.map((x) => JSON.parse(x));
+      // setRows(rowsObj);
+
+      return (
+        <DataGrid
+          className={attr.className}
+          id={attr.id}
+          rows={rowsTest}
+          onRowsChange={setRows}
+          columns={colsObj}
+        />
+      );
     },
   };
 
--- src/instructions/index.md
+++ src/instructions/index.md
@@ -8,5 +8,9 @@ A table:
 
 |   a  |   b   | c |
 | - | - | - |
-| :input{.input-test-11} | :input{.input-test-12} | :input{.input-test-13} |
+| :input{id=input-test-11 class='test-class'} | :input{.input-test-12} | :input{.input-test-13} |
 | :input{.input-test-21} | :input{.input-test-22} | :input{.input-test-23} |
+
+Datagrid test:
+
+:dataTable{class='asdf' id='afsdf' columns='[{ "key": "id", "name": "ID" } , { "key": "title", "name": "Title" }]' rows='[{ "id": "0", "title": "Example" } , { "id": "1", "title": "Demo" }]'}
