0b5673854401c16c307e22580d460b236c60c514
Zafar Yakubov <zafar.yakubov77@gmail.com>
2022-05-13 21:18:26 +0200

    Used Function() instead of eval() to parse user input. Improved styling

--- src/components/CodeParser.tsx
+++ src/components/CodeParser.tsx
@@ -1,34 +1,39 @@
-import React, {FC, useState, useEffect} from "react";
-import {Attr} from '../types'
+import "./CodeParser.css";
+
+import React, { FC, useState } from "react";
+import { Attr } from "../types";
 
 const CodeParser: FC<Attr> = (attr) => {
-    const [value, setValue] = useState("");
-    const [parsedVal, setParsedVal] = useState("");
+  const [value, setValue] = useState("");
+  const [parsedVal, setParsedVal] = useState("");
 
-    const handleValueChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setValue(e.currentTarget.value);
-    }
-    
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        
-        console.log(value);
-        try {
-            setParsedVal(eval(value));
-        } catch (err) {
-            console.log(err);
-        }
-    }
+  const handleValueChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.currentTarget.value);
+  };
 
-    return (
-        <div className="code-parser">
-            <form onSubmit={handleSubmit}>
-                <textarea value={value} onChange={handleValueChange} />
-                <input type="submit" value="Parse" className="btn btn-primary" />
-            </form>
-            <textarea value={parsedVal} readOnly />
-        </div>
-    )
-}
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      setParsedVal(Function("return " + value)());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="code-parser">
+      <form id="parser-form" onSubmit={handleSubmit}>
+        <textarea
+          id="parser-input"
+          value={value}
+          onChange={handleValueChange}
+        />
+        <input type="submit" value="Evaluate" className="btn btn-primary" />
+      </form>
+      <textarea id="parser-output" value={parsedVal} readOnly />
+    </div>
+  );
+};
 
 export default CodeParser;
